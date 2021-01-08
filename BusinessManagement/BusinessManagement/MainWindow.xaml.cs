5fd1d0afdf06759ada0bf3dd6c4865b522fb4e2f
unknown <18520358@gm.uit.edu.vn>
2021-01-08 13:30:25 +0700

    Giao Dien

--- BusinessManagement/BusinessManagement/MainWindow.xaml.cs
+++ BusinessManagement/BusinessManagement/MainWindow.xaml.cs
@@ -20,9 +20,69 @@ namespace BusinessManagement
     /// </summary>
     public partial class MainWindow : Window
     {
+        void NhanVienLogin()
+        {
+            btnAddNV.Visibility = Visibility.Collapsed;
+            btnQLNV.Visibility = Visibility.Collapsed;
+            btnQLPhongBan.Visibility = Visibility.Collapsed;
+            btnQLGioLam.Visibility = Visibility.Collapsed;
+            btnDGKN.Visibility = Visibility.Collapsed;
+            btnTKNV.Visibility = Visibility.Collapsed;
+            btnYeuCauGD.Visibility = Visibility.Collapsed;
+            btnXemYeuCau.Visibility = Visibility.Collapsed;
+        }
+        void QuanLyLogin()
+        {
+            btnXemYeuCau.Visibility = Visibility.Collapsed;
+        }
+        void GiamDocLogin()
+        {
+            btnYeuCauGD.Visibility = Visibility.Collapsed;
+        }
         public MainWindow()
         {
             InitializeComponent();
+            QuanLyLogin();
+        }
+
+        private void btnDonHang_Click(object sender, RoutedEventArgs e)
+        {
+            spMain.Children.Clear();
+            DonHang donhang = new DonHang();
+            spMain.Children.Add(donhang);
+        }
+
+        private void bntKhachHang_Click(object sender, RoutedEventArgs e)
+        {
+            spMain.Children.Clear();
+            KhachHang khachhang = new KhachHang();
+            spMain.Children.Add(khachhang);
+        }
+
+        private void btnHoaDon_Click(object sender, RoutedEventArgs e)
+        {
+            spMain.Children.Clear();
+            HoaDon hoadon = new HoaDon();
+            spMain.Children.Add(hoadon);
+        }
+
+        private void btnHangHoa_Click(object sender, RoutedEventArgs e)
+        {
+            spMain.Children.Clear();
+            HangHoa hanghoa = new HangHoa();
+            spMain.Children.Add(hanghoa);
+        }
+
+        private void btnCaNhan_Click(object sender, RoutedEventArgs e)
+        {
+            spMain.Children.Clear();
+            CaNhan canhan = new CaNhan();
+            spMain.Children.Add(canhan);
+        }
+
+        private void btnDangXuat_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
