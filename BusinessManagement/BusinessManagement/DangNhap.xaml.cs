a0a44683e320b4ff74564a9dafa199348b1cfb99
unknown <18520358@gm.uit.edu.vn>
2021-01-08 14:03:57 +0700

    DangNhapGUI

---
+++ BusinessManagement/BusinessManagement/DangNhap.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BusinessManagement
+{
+    /// <summary>
+    /// Interaction logic for DangNhap.xaml
+    /// </summary>
+    public partial class DangNhap : Window
+    {
+        public DangNhap()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mn = new MainWindow();
+            mn.Show();
+            this.Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
