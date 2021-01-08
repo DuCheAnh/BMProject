fa8917f16519c7259f9ed2382a12205caa54adbf
unknown <18520358@gm.uit.edu.vn>
2021-01-08 19:47:59 +0700

    UIGiamDoc

---
+++ BusinessManagement/BusinessManagement/CTNhanVien.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BusinessManagement
+{
+    /// <summary>
+    /// Interaction logic for CTNhanVien.xaml
+    /// </summary>
+    public partial class CTNhanVien : Window
+    {
+        public CTNhanVien()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDoiChucVu_Click(object sender, RoutedEventArgs e)
+        {
+            DoiChucVu dcv = new DoiChucVu();
+            dcv.Show();
+        }
+
+        private void btnThemHopDong_Click(object sender, RoutedEventArgs e)
+        {
+            ThemHopDong thd = new ThemHopDong();
+            thd.Show();
+        }
+    }
+}
---
+++ BusinessManagement/BusinessManagement/ThemPhongBan.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BusinessManagement
+{
+    /// <summary>
+    /// Interaction logic for ThemPhongBan.xaml
+    /// </summary>
+    public partial class ThemPhongBan : Window
+    {
+        public ThemPhongBan()
+        {
+            InitializeComponent();
+        }
+
+        private void tbSDTPB_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space) e.Handled = true;
+        }
+
+        private void tbSDTPB_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+        }
+
+        private void btnThemPB_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
