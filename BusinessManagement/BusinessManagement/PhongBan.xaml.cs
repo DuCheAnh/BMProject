402735a331daf7a3ea59c92472e32c68d4dacf6d
DemoZ101 <66673111+DemoZ101@users.noreply.github.com>
2021-01-09 20:22:28 +0700

    Add UI

---
+++ BusinessManagement/BusinessManagement/PhongBan.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BusinessManagement
+{
+    /// <summary>
+    /// Interaction logic for PhongBan.xaml
+    /// </summary>
+    public partial class PhongBan : UserControl
+    {
+        public PhongBan()
+        {
+            InitializeComponent();
+        }
+        private void btnThongKe_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        private void btnView_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
